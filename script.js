248157d5b8bd69c569aec4a87d97fec6dba93d42
gaurav234 <91665015+gauravjoshi34@users.noreply.github.com>
2025-08-07 22:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const byteSize = (str) => {
   // write your code here
+	if (!str) return 0; // handle empty string
+  
+  // Use TextEncoder to encode string to UTF-8 bytes
+  const encoder = new TextEncoder();
+  const bytes = encoder.encode(str);
+  return bytes.length;
+	
+	
 };
 
 // Do not change the code below
